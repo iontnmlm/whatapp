2afddbc04613936fbc1d5416fdd387ac3f9a7fd1
Eliyahu Tronto <a3938081@gmail.com>
2022-06-09 02:20:15 +0300

    delete window

--- src/main/java/RobotWhatsApp.java
+++ src/main/java/RobotWhatsApp.java
@@ -8,30 +8,27 @@ public class RobotWhatsApp {
     public static final int Window_Width = 400;
     private static final int Window_Height = 300;
 
+
     public static void main(String[] args) {
-        Window();
+        button();
     }
 
-    public static void Window() {
-        JFrame frame = new JFrame();
-        frame.setLayout(null);
-        frame.setBounds(0, 0, Window_Width, Window_Height);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setVisible(true);
 
-        JButton button = new JButton("Open the WhatApp");
-        button.setBounds(40, 100, Window_Height, Window_Height/5);
-        button.setFont(new Font("Arial", Font.BOLD, 18));
-        frame.add(button);
-        frame.setVisible(true);
-        button.addActionListener((even) -> {
-            frame.dispose();
-            OpenTheWhatsApp();
-        });
-    }
+    public static void button() {
+        OpenWindow openWindow = new OpenWindow(Window_Width,Window_Height);
+            JButton button = new JButton("Open the WhatApp");
+            button.setBounds(40, 100, Window_Height, Window_Height/5);
+            button.setFont(new Font("Arial", Font.BOLD,18 ));
+            openWindow.add(button);
+            openWindow.setVisible(true);
+            button.addActionListener((even) -> {
+                openWindow.dispose();
+                openTheWhatsApp();
+            });
+        }
 
-    public static void OpenTheWhatsApp() {
+
+    public static void openTheWhatsApp() {
         System.setProperty(
                 "webdriver.chrome.driver",
                 "C:\\\\Users\\\\Eliyahu toronto\\\\Dropbox\\\\PC\\\\Downloads\\\\Eli\\\\chromedriver.exe"    );
