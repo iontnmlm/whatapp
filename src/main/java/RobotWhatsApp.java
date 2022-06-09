06f83c71fa5c058e54b5ffb09c6398352fa8042a
Eliyahu Tronto <a3938081@gmail.com>
2022-06-09 19:59:42 +0300

    we finish

--- src/main/java/RobotWhatsApp.java
+++ src/main/java/RobotWhatsApp.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public class RobotWhatsApp {
 
-    public static final int Window_Width = 400 , Window_Height = 300;
+    public static final int Window_Width = 400, Window_Height = 300;
 
     public static void main(String[] args) {
         button();
@@ -13,23 +13,23 @@ public class RobotWhatsApp {
 
 
     public static void button() {
-        OpenWindow openWindow = new OpenWindow(Window_Width,Window_Height);
-            JButton button = new JButton("Open the WhatApp");
-            button.setBounds(40, 100, Window_Height, Window_Height/5);
-            button.setFont(new Font("Arial", Font.BOLD,18 ));
-            openWindow.add(button);
-            openWindow.setVisible(true);
-            button.addActionListener((even) -> {
-                openWindow.dispose();
-                openTheWhatsApp();
-            });
-        }
+        OpenWindow openWindow = new OpenWindow(Window_Width, Window_Height);
+        JButton button = new JButton("Open the WhatApp");
+        button.setBounds(40, 100, Window_Height, Window_Height / 5);
+        button.setFont(new Font("Arial", Font.BOLD, 18));
+        openWindow.add(button);
+        openWindow.setVisible(true);
+        button.addActionListener((even) -> {
+            openWindow.dispose();
+            openTheWhatsApp();
+        });
+    }
 
 
     public static void openTheWhatsApp() {
         System.setProperty(
                 "webdriver.chrome.driver",
-                "C:\\Users\\User\\Downloads\\chromedriver_win32\\chromedriver.exe");
+                "C:\\\\Users\\\\Eliyahu toronto\\\\Dropbox\\\\PC\\\\Downloads\\\\Eli\\\\chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://web.whatsapp.com/");
@@ -39,7 +39,7 @@ public class RobotWhatsApp {
             while (true) {
                 boolean firstPage = driver.getPageSource().contains("תיבת טקסט להזנת החיפוש");
                 if (firstPage) {
-                    Connection connection = new Connection(driver);
+                    UserInputWindow userInputWindow = new UserInputWindow(driver);
                     break;
                 }
 
